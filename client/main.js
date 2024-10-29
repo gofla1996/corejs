60d02041d8ec8e9fcc7a0bd1919bad91e723db19
hyerim <110741@daum.net>
2024-10-29 13:39:11 +0900

    first commit

---
+++ client/main.js
@@ -0,0 +1,12 @@
+// /* eslint no-unused-vars: warn */
+
+// mjs : 웹 브라우저에서 작동하는 일반 자바스크립트
+// js : node.js에서 작동하는 자바스크립트
+// 그냥 js로 설정하면 둘 다 작동 가능
+// cjs : node.js에서 작동하는 commonJS
+
+const a = 'hello';
+
+function sum() {
+  console.log(1 + 2);
+}
