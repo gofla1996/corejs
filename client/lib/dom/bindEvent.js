18b0f0f62caf4b09429c0950b373bd49b3736c79
hyerim <110741@daum.net>
2024-11-19 15:34:37 +0900

    bindEvent.js

---
+++ client/lib/dom/bindEvent.js
@@ -0,0 +1,7 @@
+
+export function bindEvent(node,type,handler){
+  if(isString(node)) node = getNode(node);
+  node.addEventListener(type,handler);
+
+  return ()=> node.removeEventListener(type,handler);
+}
