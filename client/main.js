f81255c916adc33b05471dbaf847a67e983c63f5
hyerim <110741@daum.net>
2024-11-27 16:07:21 +0900

    피그마 연계 컴포넌트 만들기

--- client/main.js
+++ client/main.js
@@ -1,20 +1,25 @@
-import {Button} from './components/Button/Button.js';
+import { Button } from './components/Button/Button.js';
+import { Card } from './components/Card/Card.js';
+import { Counter } from './components/Counter/Counter.js';
 
 
 const app = document.getElementById('app');
 
-
-function defineElements(){
+function defineElements() {
   customElements.define('custom-button', Button);
 
+  customElements.define('custom-card', Card);
+
+  customElements.define('custom-counter', Counter);
 
 }
-
 
 defineElements();
 
 const buttonElement = document.createElement('custom-button');
 
 
-app.append(buttonElement);
 
+
+
+// app.append(buttonElement);
