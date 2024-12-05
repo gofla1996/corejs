352d9d66afbc3cb663a92359c59febe21a2261c5
hyerim <110741@daum.net>
2024-12-05 18:11:30 +0900

    회원가입 로그인 구현

--- src/@types/type.d.ts
+++ src/@types/type.d.ts
@@ -1,3 +1,6 @@
+
+
+
 export interface Product {
   items:      Item[];
   page:       number;
@@ -19,3 +22,30 @@ export interface Item {
   updated:        Date;
   [key:string]:   string
 }
+
+
+export interface Record {
+  avatar: string;
+  collectionId: string;
+  collectionName: string;
+  created: string;
+  email: string;
+  emailVisibility: boolean;
+  id: string;
+  name: string;
+  updated: string;
+  verified: boolean;
+}
+
+
+export interface Auth{
+  isAuth:boolean;
+  token:string;
+  user:Record
+}
+
+
+
+
+
+
--- src/Layout/Header.ts
+++ src/Layout/Header.ts
@@ -1,43 +1,84 @@
-import { LitElement, html, css, CSSResultGroup } from "lit";
-import { customElement } from "lit/decorators.js";
+import { css, CSSResultGroup, html, LitElement } from "lit";
+import { customElement, state } from "lit/decorators.js";
+import Swal from "sweetalert2";
+import { Auth } from "../@types/type";
 import resetCSS from "./resetCSS";
+import pb from "../api/pocketbase";
 
 
-@customElement('c-header')
+
+@customElement("c-header")
+
 
 class Header extends LitElement {
 
+  @state() private loginData:Auth = {} as Auth;
 
-  static styles:CSSResultGroup = [
+  static styles: CSSResultGroup = [
     resetCSS,
     css`
-      header{
+      header {
         display: flex;
         justify-content: space-between;
         background-color: white;
         color: black;
         padding: 1rem;
-      
-        .logo{
+
+        .logo {
           display: flex;
           align-items: center;
           gap: 0.3rem;
         }
 
-        nav{
+        nav {
           display: flex;
           align-items: center;
 
-          ul{
+          ul {
             display: flex;
             gap: 1rem;
           }
         }
       }
-    `
-  ]
+    `,
+  ];
 
-  render(){
+
+  connectedCallback(){
+    super.connectedCallback();
+    this.fetchData();
+  }
+
+  fetchData(){
+    const auth = JSON.parse(localStorage.getItem('auth') ?? "{}");
+    this.loginData = auth;
+  }
+
+  handleLogout(e:Event){
+    e.preventDefault()
+    
+    Swal.fire({
+      title:'로그아웃',
+      text:'로그아웃 하시겠습니까?',
+      icon:"question",
+      showCancelButton:true,
+      confirmButtonText:'로그아웃'
+    })
+    .then(({isConfirmed})=>{
+      
+      if(isConfirmed){
+
+        localStorage.removeItem('auth');
+        pb.authStore.clear();
+        // this.loginData.isAuth = false;
+        // this.requestUpdate()
+        location.reload()
+      }
+    })
+  }
+
+  render() {
+    const { isAuth, user } = this.loginData;
 
     return html`
       <header>
@@ -50,14 +91,19 @@ class Header extends LitElement {
             <li><a href="/">About</a></li>
             <li><a href="/src/pages/product/">Product</a></li>
             <li><a href="/">Contact</a></li>
-            <li><a href="/">Login</a></li>
+            <li>
+              ${!isAuth
+                ? html`<a href="/src/pages/login/">Login</a>`
+                : html`
+                    <div>
+                      <span>${user.name}님</span>
+                      <a @click=${this.handleLogout} href="/">Logout</a>
+                    </div>
+                  `}
+            </li>
           </ul>
         </nav>
       </header>
-    
-    `
+    `;
   }
-
 }
-
-
